3bc468c25059dbc5db25656d9b3c141bd9df5080
Arthur Henrique Fagundes <arthurhfagundes6@gmail.com>
2025-10-21 07:35:34 -0300

    Exercício corrigido Mód. 25

--- exercicio.js
+++ exercicio.js
@@ -1,20 +1,56 @@
-let notasAlunos = new Map();
+// let notasAlunos = new Map();
 
-notasAlunos.set('joao', 10)
-notasAlunos.set('maria', 5)
-notasAlunos.set('carlos', 4)
-notasAlunos.set('paola', 7)
-notasAlunos.set('kauan', 6)
-notasAlunos.set('daniela', 8)
-notasAlunos.set('marcos', 9)
-notasAlunos.set('julia', 6)
+// notasAlunos.set('joao', 10)
+// notasAlunos.set('maria', 5)
+// notasAlunos.set('carlos', 4)
+// notasAlunos.set('paola', 7)
+// notasAlunos.set('kauan', 6)
+// notasAlunos.set('daniela', 8)
+// notasAlunos.set('marcos', 9)
+// notasAlunos.set('julia', 6)
 
 
-for (let [aluno, notas] of notasAlunos.entries()) {
-    if (notas >= 6) {
-        console.log(`${aluno} tirou:`, notas)
+// for (let [aluno, notas] of notasAlunos.entries()) {
+//     if (notas >= 6) {
+//         console.log(`${aluno} tirou:`, notas)
+//     }
+//     else {
+//         console.log(`${aluno} não passou!`)
+//     }
+// }
+
+const alunos = ['Bruno', 'Julia']
+
+
+const alunosNota = alunos.map(function(itemAtual) { 
+    return itemAtual = { 
+        nome: itemAtual, 
+        nota: 10
     }
-    else {
-        console.log(`${aluno} não passou!`)
-    }
+})
+
+alunosNota.push({ 
+    nome: 'Carla',
+    nota: 5,
+})
+
+alunosNota.push({ 
+    nome: 'João',
+    nota: 3,
+})
+
+alunosNota.push({ 
+    nome: 'Aline',
+    nota: 6,
+})
+
+console.log(alunosNota) 
+
+
+function filtraQuemPassou(aluno) { 
+    return aluno.nota >= 6;
 }
+const quemPassou = alunosNota.filter(filtraQuemPassou)
+
+
+console.log(quemPassou) 
