d59ea93598dd7cdc1e93cf3f10588aed89804f78
Arthur Henrique Fagundes <arthurhfagundes6@gmail.com>
2025-10-26 18:15:17 -0300

    Exercício Typescript

---
+++ exercicio.ts
@@ -0,0 +1,17 @@
+function multiplicarDoisNumeros(primeiroNumero:number, segundoNumero:number): void {
+    const resultado = primeiroNumero * segundoNumero;
+    
+    console.log(`${primeiroNumero} X ${segundoNumero} = ${resultado}`)
+}
+multiplicarDoisNumeros(10, 20);
+
+
+function saudacaoAhAlguem(nome:string) {
+    return(`Olá ${nome}!`);
+}
+saudacaoAhAlguem("Matheus")
+
+
+
+console.log(multiplicarDoisNumeros)
+console.log(saudacaoAhAlguem)
